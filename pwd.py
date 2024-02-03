50aa4f56303be3e3393718237685f3dda953ba45
mickoissicko <defualtusermick@gmail.com>
2024-02-03 16:35:06 +0100

    fixed incorrect path

--- pwd.py
+++ pwd.py
@@ -4,7 +4,7 @@ import os
 
 def main():
     current_working_directory = os.getcwd()
-    with open('../config/working.directory', 'w') as file:
+    with open('config/working.directory', 'w') as file:
         file.write(current_working_directory)
 
 if __name__ == "__main__":
