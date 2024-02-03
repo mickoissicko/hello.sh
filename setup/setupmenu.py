ea0ca6d929562f5393177be448f0c45e671069b6
mickoissicko <defualtusermick@gmail.com>
2024-02-03 19:38:38 +0100

    fixed all the bugs, works flawlessly before it wouldnt correctly read/write/modify text files or read path

--- setup/setupmenu.py
+++ setup/setupmenu.py
@@ -16,42 +16,23 @@ def print_file_content(file_path):
 def configure_fetch_command():
     fetch_path = input("Enter the *Fetch command: ")
     fetch_content = f"# Startup command. Default is ScreenFetch -- you may change this.\n\n$NFETCH == {fetch_path}\n"
-    write_file("../config/neo.fetch", fetch_content)
+    write_file("config/neo.fetch", fetch_content)
 
 def activate():
-    os.system('python ../pwd.py')
 
-    with open('../config/working.directory', 'r') as wd_file:
-        working_directory = wd_file.read().strip()
+    dot_files = input("Enter the path to your shell-DOTFILES: ")
 
-    dotfile_path = input("Enter the path to your shell-DOTFILE: ")
+    current_working_directory = os.getcwd()
+    with open('config/working.directory', 'w') as file:
+        file.write(f'WD={current_working_directory}\n')
+        file.write(f'DF={dot_files}\n')
 
-    username = getpass.getuser()
-    dotfile_path = dotfile_path.replace('~', f'/home/{username}')
-
-    rc_toml_path = '../config/rc.toml'
-
-    with open(rc_toml_path, 'r') as file:
-        contents = file.read()
-
-    contents = contents.replace('$SHELL ==', f'$SHELL == "{dotfile_path}"')
-
-    with open(rc_toml_path, 'w') as file:
-        file.write(contents)
-
-    with open(dotfile_path, 'a') as dotfile:
-        dotfile.write(f'\n# HELLOSHELL CONFIGURATION\npython {working_directory}/scripts/welcomeshell.py')
+    with open(dot_files, 'a') as file:
+        file.write(f'# HelloShell Startup Configuration\n')
+        file.write(f'python {current_working_directory}/scripts/welcomeshell.py\n')
 
 def deactivate():
-    rc_toml_path = '../config/rc.toml'
-
-    with open(rc_toml_path, 'r') as file:
-        contents = file.read()
-
-    contents = contents.replace('$SHELL ==', '$SHELL == ~/.bashrc')
-
-    with open(rc_toml_path, 'w') as file:
-        file.write(contents)
+    pass
 
 def main():
     while True:
