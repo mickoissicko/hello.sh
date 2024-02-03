7234d261de2bdcd0cd24b67deb0a81831d21fb63
mickoissicko <defualtusermick@gmail.com>
2024-02-03 12:30:55 +0100

    added neofetch configuration file

--- scripts/welcomeshell.py
+++ scripts/welcomeshell.py
@@ -4,14 +4,31 @@ import os
 import getpass
 import subprocess
 
+config_path = '../config/term.txt'
+config2_path = '../config/neo.fetch'
+
+try:
+    with open(config_path, 'r') as config_file:
+        terminal = config_file.read().strip()
+except FileNotFoundError:
+    terminal = 'konsole'
+command = [terminal, '--hold', '-e', 'python3', 'scripts/btop.py']
+
+try:
+    with open(config2_path, 'r') as config_file:
+        command = config_file.read().strip()
+
+except FileNotFoundError:
+    command = 'neofetch'
+
 def howdy():
     os.system('clear')
-    os.system('screenfetch')
+    subprocess.run(command, shell=True)
 
     whoami = getpass.getuser()
     terminal_width = os.get_terminal_size().columns
 
-    sup = f"Hello, {whoami}!"
+    sup = f"Welcome back, {whoami}!"
     padding = (terminal_width - len(sup)) // 2
 
     border = "=" * terminal_width
