16c91e789be5e649170145837e7c22fb979f5fd2
mickoissicko <defualtusermick@gmail.com>
2024-02-03 15:52:29 +0100

    going to rewrite it

--- setup/setupmenu.py
+++ setup/setupmenu.py
@@ -17,30 +17,18 @@ def configure_fetch_command():
     fetch_content = f"# Startup command. Default is ScreenFetch -- you may change this.\n\n$NFETCH == {fetch_path}\n"
     write_file("../config/neo.fetch", fetch_content)
 
-def configure_dotfile_path():
-    dotpath = input("Enter the path to your shell-DOTFILE: ")
+def activate():
+    pass
 
-    with open('../config/rc.toml', 'r') as file:
-        contents = file.read()
-
-    contents = contents.replace('$SHELL == ~/.bashrc', f'$SHELL == {dotpath}')
-
-    with open('../config/rc.toml', 'w') as file:
-        file.write(contents)
-
-    current_path = os.getcwd()
-    final_path = os.path.join(current_path, 'scripts', 'welcomeshell.py')
-
-    dotpath_without_setup = os.path.join(os.path.dirname(dotpath), os.path.basename(dotpath).replace('setup', ''))
-    
-    with open(dotpath_without_setup, 'a') as file:
-        file.write(f'# ADDED BY HELLOSHELL DO NOT ADD ANYTHING UNDER IT\npython {final_path}\n')
+def deactivate():
+    pass
 
 def main():
     while True:
         print("\nMenu:")
         print("[1] Configure *Fetch command")
-        print("[2] Configure DOTFILE path")
+        print("[2] Activate")
+        print("[3] Deactivate")
         print("[X] Quit")
 
         option = input("Enter your choice: ").upper()
@@ -51,7 +39,9 @@ def main():
         elif option == '1':
             configure_fetch_command()
         elif option == '2':
-            configure_dotfile_path()
+            activate()
+        elif option == '3':
+            deactivate()
         else:
             print("\nInvalid option. Please enter a valid option.\n")
 
