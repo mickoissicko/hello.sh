df54e02bb4c1a348f73dedc31a861c9aff64c3f4
mickoissicko <defualtusermick@gmail.com>
2024-02-03 15:29:01 +0100

    remove 'setup' from file path

--- setup/setupmenu.py
+++ setup/setupmenu.py
@@ -16,45 +16,31 @@ def configure_fetch_command():
     fetch_path = input("Enter the *Fetch command: ")
     fetch_content = f"# Startup command. Default is ScreenFetch -- you may change this.\n\n$NFETCH == {fetch_path}\n"
     write_file("../config/neo.fetch", fetch_content)
-    print("\n[1] Fetch command configured successfully.\n")
 
 def configure_dotfile_path():
-    rc_path = input("Enter the path to your RC/Prompt configuration file: ")
-    term_emulator = input("Enter preferred terminal emulator: ")
+    dotpath = input("Enter the path to your shell-DOTFILE: ")
 
-    rc_content = f"# Default presets\n# You can add a custom path\n\n&STARSHIP == ~/.config/starship.toml\n&FISH == ~/.config/fish/config.fish\n&BASH == ~/.bashrc\n&ZSH == {rc_path}\n\n# The below line determines the path. Default is ~/.bashrc.\n$SHELL == {rc_path}\n"
-    term_content = f"# The default terminal will be Konsole. You can change this.\n\n$TERM == {term_emulator}\n"
+    with open('../config/rc.toml', 'r') as file:
+        contents = file.read()
 
-    write_file("../config/rc.toml", rc_content)
-    write_file("../config/term.toml", term_content)
+    contents = contents.replace('$SHELL == ~/.bashrc', f'$SHELL == {dotpath}')
 
-    print("\n[2] DOTFILE path and Terminal emulator configured successfully.\n")
+    with open('../config/rc.toml', 'w') as file:
+        file.write(contents)
 
-def configure_automatic_start(rc_path):
-    rc_content = read_file(rc_path)
+    current_path = os.getcwd()
+    final_path = os.path.join(current_path, 'scripts', 'welcomeshell.py')
 
-    shell_line_start = rc_content.find("$SHELL == ") + len("$SHELL == ")
-    shell_line_end = rc_content.find("\n", shell_line_start)
-
-    current_shell_path = rc_content[shell_line_start:shell_line_end].strip()
-
-    print(f"\nCurrent shell path: {current_shell_path}")
-
-    new_script_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), 'scripts', 'welcomeshell.py')
-
-    new_content = f"\n# Added by the configuration script\npython {new_script_path}\n"
-
-    with open(rc_path, 'a') as rc_file:
-        rc_file.write(new_content)
-
-    print("\n[3] Automatic start configured successfully.\n")
+    dotpath_without_setup = os.path.join(os.path.dirname(dotpath), os.path.basename(dotpath).replace('setup', ''))
+    
+    with open(dotpath_without_setup, 'a') as file:
+        file.write(f'# ADDED BY HELLOSHELL DO NOT ADD ANYTHING UNDER IT\npython {final_path}\n')
 
 def main():
     while True:
         print("\nMenu:")
         print("[1] Configure *Fetch command")
-        print("[2] DOTFILE path and Terminal emulator")
-        print("[3] Configure Automatic start")
+        print("[2] Configure DOTFILE path")
         print("[X] Quit")
 
         option = input("Enter your choice: ").upper()
@@ -66,8 +52,6 @@ def main():
             configure_fetch_command()
         elif option == '2':
             configure_dotfile_path()
-        elif option == '3':
-            configure_automatic_start(rc_path)
         else:
             print("\nInvalid option. Please enter a valid option.\n")
 
