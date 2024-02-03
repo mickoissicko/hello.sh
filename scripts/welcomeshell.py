3b916e605e07b94ea20553db0cf68dad1bc6ca57
mickoissicko <defualtusermick@gmail.com>
2024-02-03 13:19:43 +0100

    accidental bar at the bottom looks cool (its a feature now) yes.. i know.. minecraft splashes

--- scripts/welcomeshell.py
+++ scripts/welcomeshell.py
@@ -3,37 +3,10 @@
 import os
 import getpass
 import subprocess
-
-config_path = '../config/term.toml'
-config2_path = '../config/neo.fetch'
-
-try:
-    with open(config2_path, 'r') as config_file:
-        nfetch_line = config_file.read().strip()
-        nfetch_command = nfetch_line.split('==', 1)[1].strip()
-except FileNotFoundError:
-    nfetch_command = 'neofetch'
-
-def read_term_config():
-    try:
-        with open(config_path, 'r') as config_file:
-            for line in config_file:
-                line = line.strip()
-                if line.startswith('$TERM =='):
-                    return line.split('==', 1)[1].strip()
-            return None
-    except FileNotFoundError:
-        return None
+import random
 
 def howdy():
     os.system('clear')
-
-    term_command = read_term_config()
-
-    if term_command:
-        subprocess.run(term_command, shell=True)
-    else:
-        print("No $TERM command specified in term.toml")
 
     whoami = getpass.getuser()
     terminal_width = os.get_terminal_size().columns
@@ -47,5 +20,41 @@ def howdy():
     print(" " * padding + sup)
     print(border)
 
-if __name__ == "__main__":
+def splash():
+    splashtext_file = "../text/splash.text"
+    
+    try:
+        with open(splashtext_file, 'r') as file:
+            phrases = file.readlines()
+        
+        random_phrase = random.choice(phrases)
+    except FileNotFoundError:
+        print(f"Error: File '{splashtext_file}' not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    terminal_width = os.get_terminal_size().columns
+
+    printmotd = f"{random_phrase.strip()}"
+    padding = (terminal_width - len(printmotd)) // 2
+
+    border = "=" * terminal_width
+
+    print(border)
+    print(" " * padding + printmotd)
+    print(border)
+
+def main():
     howdy()
+
+    try:
+        with open('neo.fetch', 'r') as file:
+            startup_command = file.read().strip()
+    except FileNotFoundError:
+        startup_command = "neofetch"
+    subprocess.run(startup_command, shell=True)
+
+    splash()
+
+if __name__ == "__main__":
+    main()
