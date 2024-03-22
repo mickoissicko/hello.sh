dbbf911023d562c90e09e6458e71daa63118d7ea
mickey <139474837+mickoissicko@users.noreply.github.com>
2024-03-22 02:07:31 +0100

    fixed for alacritty emulator

--- scripts/welcomeshell.py
+++ scripts/welcomeshell.py
@@ -5,20 +5,19 @@ import getpass
 import subprocess
 import random
 
+def print_with_border(text):
+    terminal_width = os.get_terminal_size().columns
+    border = "=" * terminal_width
+    padding = (terminal_width - len(text)) // 2
+    print(border)
+    print(" " * padding + text)
+    print(border)
+
 def howdy():
     os.system('clear')
-
     whoami = getpass.getuser()
-    terminal_width = os.get_terminal_size().columns
-
     sup = f"Welcome back, {whoami}!"
-    padding = (terminal_width - len(sup)) // 2
-
-    border = "=" * terminal_width
-
-    print(border)
-    print(" " * padding + sup)
-    print(border)
+    print_with_border(sup)
 
 def splash():
     script_dir = os.path.dirname(os.path.realpath(__file__))
@@ -28,24 +27,12 @@ def splash():
         with open(splashtext_file, 'r') as file:
             phrases = file.readlines()
 
-        random_phrase = random.choice(phrases)
+        random_phrase = random.choice(phrases).strip()
+        print_with_border(random_phrase)
     except FileNotFoundError:
         print(f"Error: File '{splashtext_file}' not found.")
-        return
-
-    except Exception:
-        exit
-
-    terminal_width = os.get_terminal_size().columns
-
-    printmotd = f"{random_phrase.strip()}"
-    padding = (terminal_width - len(printmotd)) // 2
-
-    border = "=" * terminal_width
-
-    print(border)
-    print(" " * padding + printmotd)
-    print(border)
+    except Exception as e:
+        print("An error occurred:", e)
 
 def main():
     howdy()
@@ -67,5 +54,6 @@ def main():
     subprocess.run(startup_command, shell=True)
 
     splash()
+
 if __name__ == "__main__":
     main()
